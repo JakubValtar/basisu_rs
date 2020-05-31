e22c5b99a91fd963b73cac01d0712ef11c4a1d5b
Jakub Valtar <jakub.valtar@gmail.com>
2020-05-31 15:09:28 +0200

    Added a test for bit reader

--- src/bitreader.rs
+++ src/bitreader.rs
@@ -41,3 +41,42 @@ impl<'a> BitReaderLSB<'a> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bitreader() {
+        let pattern = 0x5555_5555_5555_5555u64;
+        for i in 0..16 {
+            let xor_mask =
+                ((0i16 - ((i >> 3) & 0x1)) as u16 as u64) << 3*16 |
+                ((0i16 - ((i >> 2) & 0x1)) as u16 as u64) << 2*16 |
+                ((0i16 - ((i >> 1) & 0x1)) as u16 as u64) << 1*16 |
+                ((0i16 - ((i >> 0) & 0x1)) as u16 as u64) << 0*16;
+            let data = pattern ^ xor_mask;
+            let bytes = data.to_le_bytes();
+            for len in 0..32 {
+                for offset in 0..32 {
+                    let mut reader = BitReaderLSB::new(&bytes);
+                    let actual = reader.read(offset);
+                    let expected = (data & ((1u64 << offset) - 1)) as u32;
+                    assert_eq!(
+                        actual, expected,
+                        "offset value mismatch, left: {:b}, right: {:b}, off: {}, len: {}, data: {:b}",
+                        actual, expected, offset, len, data
+                    );
+
+                    let actual = reader.read(len);
+                    let expected = ((data >> offset) & ((1u64 << len) - 1)) as u32;
+                    assert_eq!(
+                        actual, expected,
+                        "value mismatch, left: {:b}, right: {:b}, off: {}, len: {}, data: {:b}",
+                        actual, expected, offset, len, data
+                    );
+                }
+            }
+        }
+    }
+}
