410dab989bcb02d5fba6906b4074bc2deb316d17
Jakub Valtar <jakub.valtar@gmail.com>
2020-08-16 21:47:53 +0200

    Added corpus test for UASTC -> BC7

--- tests/corpus_tests.rs
+++ tests/corpus_tests.rs
@@ -20,6 +20,15 @@ fn test_uastc_to_astc() {
 }
 
 #[test]
+fn test_uastc_to_bc7() {
+    iterate_textures_uastc(|case| {
+        let decoded = basisu::read_to_bc7(&case.basis).unwrap();
+        assert_eq!(decoded.len(), 1);
+        compare_ktx(&case.bc7_rgba, &decoded[0]).unwrap();
+    });
+}
+
+#[test]
 fn test_etc1s_to_rgba() {
     iterate_textures_etc1s(|case| {
         let decoded = basisu::read_to_rgba(&case.basis).unwrap();
